0278b4acb605c0766337b0b21d6947e64dfda0e3
Ananay <73402363+FTS18@users.noreply.github.com>
2025-09-17 11:10:47 +0530

    ok

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -52,7 +52,7 @@ export const Hero = ({ onGetStartedClick }: HeroProps) => {
 
       <div className="relative z-10 max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 text-center mb-12">
         <div className="max-w-6xl mx-auto">
-          <div className="flex justify-center mb-8 mt-20 lg:mt-40">
+          <div className="flex justify-center mb-8 mt-12 lg:mt-24">
             <div className="inline-flex items-center px-4 py-2 sm:px-4 sm:py-2 rounded-full bg-green-500/10 border border-green-500/20 shadow-lg">
               <Sparkles className="w-4 h-4 sm:w-5 sm:h-5 text-green-500 mr-3" />
               <span className="text-sm sm:text-base font-semibold text-green-500">
