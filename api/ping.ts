3f9df3c020fa6e19ac2c779d514ced7ed4f8b98d
fts18 <anandsagardubey71568@gmail.com>
2026-07-16 23:43:20 +0530

    chore: test firestore import

--- api/ping.ts
+++ api/ping.ts
@@ -1,1 +1,1 @@
-import { getApps } from "firebase-admin/app"; export default function handler(req, res) { res.status(200).json({ ok: true, msg: "pong v3", apps: getApps().length }); }
+import { getFirestore } from "firebase-admin/firestore"; export default function handler(req, res) { res.status(200).json({ ok: true, msg: "pong v4 firestore" }); }
